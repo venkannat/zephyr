409464052e5d221b303d51bc16d66400127e326f
Naunidh Dua <naunidh.dua@amd.com>
2025-09-04 12:55:51 +0100

    VSW-380: Change coredump region to just kernel ram
    This excludes redundant information such as text and and ro area

--- subsys/debug/coredump/coredump_memory_regions.c
+++ subsys/debug/coredump/coredump_memory_regions.c
@@ -16,7 +16,7 @@
 
 #ifdef CONFIG_DEBUG_COREDUMP_MEMORY_DUMP_LINKER_RAM
 struct z_coredump_memory_region_t __weak z_coredump_memory_regions[] = {
-	{(uintptr_t)&_image_ram_start, (uintptr_t)&_image_ram_end},
+	{(uintptr_t)&__kernel_ram_start, (uintptr_t)&__kernel_ram_end},
 	{0, 0} /* End of list */
 };
 #endif
